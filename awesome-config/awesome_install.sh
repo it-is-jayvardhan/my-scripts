029b943a870ac5a9549cc1ad4e9e3e764f41f2d0
it-is-jayvardhan <grprashanthreddy@gmail.com>
2023-11-18 13:59:49 +0530

    modified from arch installer to fedora installer

--- awesome-config/awesome_install.sh
+++ awesome-config/awesome_install.sh
@@ -1,9 +1,9 @@
-sudo pacman -Sy awesome thunar terminator
+sudo dnf install awesome thunar terminator
 
 echo "select wallpaper to be applied"
 
 
 sudo mkdir ~/.config/awesome
-cp rc.lua ~/.config/awesome/
+sudo cp rc.lua ~/.config/awesome/
 awesome -k
 awesome restart
